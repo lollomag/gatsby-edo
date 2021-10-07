dffa150c4886076354d66c8221192a3586f2d2c2
lorenzo cadente <lorenzo.cadente@accenture.com>
2021-10-07 19:39:27 +0200

    progress

---
+++ src/components/header/header.js
@@ -0,0 +1,47 @@
+import * as React from "react"
+import PropTypes from "prop-types"
+// import { Link } from "gatsby"
+
+import logo from '../../images/logo.png';
+
+const Header = ({ siteTitle }) => (
+  <header>
+    <div className="header-desktop">
+      <a href="index.html">
+        <img className="logo" src={logo} width="100" height="100" alt="my logo"></img>
+      </a>
+      <nav>
+        <a href="index.html" className="nav-item {{#if active-1}}active{{/if}}">Home</a>
+        <a href="foto.html" className="nav-item {{#if active-2}}active{{/if}}">Photos</a>
+        <a href="video.html" className="nav-item {{#if active-3}}active{{/if}}">Cinema</a>
+        <a href="about.html" className="nav-item {{#if active-4}}active{{/if}}">About</a>
+      </nav>
+  </div>
+      <div className="header-mobile">
+        <div className="mobile-head">
+          <a href="index.html">
+            <img className="logo" src={logo} width="30" height="30" alt="my logo"></img>
+      </a>
+            <span className="hamburger-mobile material-icons-outlined">menu</span>
+    </div>
+          <div className="mobile-navigation">
+            <nav>
+              <a href="index.html" className="nav-item {{#if active-1}}active{{/if}}">Home</a>
+              <a href="foto.html" className="nav-item {{#if active-2}}active{{/if}}">Photos</a>
+              <a href="video.html" className="nav-item {{#if active-3}}active{{/if}}">Cinema</a>
+              <a href="about.html" className="nav-item {{#if active-4}}active{{/if}}">About</a>
+            </nav>
+          </div>
+        </div>
+</header>
+)
+
+Header.propTypes = {
+        siteTitle: PropTypes.string,
+}
+
+Header.defaultProps = {
+        siteTitle: ``,
+}
+
+export default Header
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,26 +1,43 @@
 import * as React from "react"
 import { Link, graphql } from "gatsby"
 // import { StaticImage } from "gatsby-plugin-image"
+import Plyr from 'plyr';
 
 import Layout from "../components/layout"
-import Seo from "../components/seo"
 
 const IndexPage = ({ data }) => {
   console.log(data);
-  
+
   return (
     <Layout>
-      <Seo title="Home" />
-      <h1>Hi people</h1>
-      <p>Welcome to your new Gatsby site.</p>
-      <p>{data.strapiHomePage.vimeoId}</p>
+      <div className="container">
+        <div className="home-video mb-80">
+          {/* <div id="player" data-plyr-provider="vimeo" data-plyr-embed-id="529441048"></div> */}
+        </div>
+        <div className="bio-carousel"></div>
+        <div className="fx-center-between mt-60">
+          <div className="social-link">
+            <a href="https://it.linkedin.com/in/edoardo-marcuzzi-46a347200" target="_blank" rel="noreferrer" className="social-item" title="Link open a new page on my linkedin account"><span
+              className="icn-linkedin"></span></a>
+            <a href="https://vimeo.com/user107581401" target="_blank" rel="noreferrer" className="social-item" title="Link open a new page on my vimeo account"><span
+              className="icn-vimeo"></span></a>
+            <a href="https://www.instagram.com/edomarcuz/?hl=it" target="_blank" rel="noreferrer" className="social-item" title="Link open a new page on my instagram account"><span
+              className="icn-instagram"></span></a>
+            <a href="tel:+393492879150" className="social-item" title="Call me at the number 3492879150"><span className="icn-phone"></span></a>
+          </div>
+          <a download="CV-edoardo-marcuzzi" target="_blank" href={data.strapiHomePage.curriculum.url} className="download-btn" rel="noreferrer">
+            <span className="btn-text">Download my CV</span>
+            <span className="material-icons-outlined">file_download</span>
+          </a>
+        </div>
+      </div>
+      <p className="vimeo">{data.strapiHomePage.vimeoId}</p>
       {data.strapiHomePage.photos.map((item, i) => {
-        return <img key={i} src={item.localFile.url} alt="test"/>
+        return <img key={i} src={item.localFile.url} alt="test" />
       })}
-      
+
       <p>
         <Link to="/page-2/">Go to page 2</Link> <br />
-        <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
       </p>
     </Layout>
   )
