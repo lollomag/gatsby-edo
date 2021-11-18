170b6702ac7608c43c22c98767022005acb50f79
lollomag <lorenzo.cadente@yahoo.it>
2021-11-18 21:00:26 +0100

    test build

---
+++ src/pages/about.jsx
@@ -0,0 +1,17 @@
+import React from "react"
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+import Layout from "../components/layout"
+
+const AboutPage = () => {
+
+  return (
+    <Layout>
+      <div className="container">
+      pagina about
+      </div>
+    </Layout>
+  )
+}
+
+export default AboutPage
