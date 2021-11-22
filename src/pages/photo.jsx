a71fd7ccecb6187ad4e6be9199a2d9ec3c767a08
lollomag <lorenzo.cadente@yahoo.it>
2021-11-22 15:59:47 +0100

    gallery

--- src/pages/photo.jsx
+++ src/pages/photo.jsx
@@ -1,26 +1,47 @@
 import React from "react"
 import { graphql } from "gatsby"
 import 'bootstrap/dist/css/bootstrap.min.css';
+import 'photoswipe/dist/photoswipe.css'
+import 'photoswipe/dist/default-skin/default-skin.css'
 
+import { Gallery, Item } from 'react-photoswipe-gallery'
 import Layout from "../components/layout"
 
 const PhotoPage = ({ data }) => {
   console.log(data);
 
+  const galleryOptions = {
+    shareEl: false,
+  }
+
   return (
     <Layout>
       <div className="container">
-      <div class="gallery-wrapper">
-        <div class="section-title">Photography</div>
-        <div class="filter-wrapper photo">
+        <div class="gallery-wrapper">
+          <div class="section-title">Photography</div>
+          <div class="filter-wrapper photo">
 
-        </div>
-        {data.allStrapiPhoto.edges.map((item, i) => {
-            return <div key={i}>
-              <img src={item.node.image.url} alt=""></img>
+          </div>
+          <Gallery options={galleryOptions}>
+            <div className="row photoswipe-gallery">
+              {data.allStrapiPhoto.edges.map((item, i) => {
+                return <div className="col-md-4" key={i}>
+                  {/* <img src={item.node.image.url} alt=""></img> */}
+                  <Item
+                    original={item.node.image.url}
+                    thumbnail={item.node.image.url}
+                    width="80%"
+                    height="768"
+                  >
+                    {({ ref, open }) => (
+                      <img ref={ref} onClick={open} src={item.node.image.url} />
+                    )}
+                  </Item>
+                </div>
+              })}
             </div>
-          })}
-      </div>
+          </Gallery>
+        </div>
       </div>
     </Layout>
   )
